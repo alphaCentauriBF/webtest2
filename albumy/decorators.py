52a0838002f6947d0050145d807bf10851853131
feng <1614404677@qq.com>
2019-03-22 20:45:20 +0800

    Update decorators.py

--- albumy/decorators.py
+++ albumy/decorators.py
@@ -10,7 +10,7 @@ from functools import wraps
 from flask import Markup, flash, url_for, redirect, abort
 from flask_login import current_user
 
-
+#hey
 def confirm_required(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
